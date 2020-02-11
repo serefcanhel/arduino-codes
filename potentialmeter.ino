1272c7f612f6e7f4452c9fc813b9a22aef6779c2
serefcanhel <55743293+serefcanhel@users.noreply.github.com>
2020-02-11 13:31:46 +0300

    Add files via upload
    A potentialmeter code for arduino uno

---
+++ potentialmeter.ino
@@ -0,0 +1,17 @@
+int potPin=A0;
+int ledPin=6;
+int ledPin_2=5;
+void setup() {
+  Serial.begin(9600);
+  Serial.print("output value : ");
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  double value=analogRead(potPin);
+  Serial.println(value*5/1023);
+  delay(100);
+  analogWrite(ledPin, value*255/1023);
+
+}
