5f520f76131b4d0c658e572f067b6c03743c57ed
serefcanhel <55743293+serefcanhel@users.noreply.github.com>
2020-02-11 13:32:22 +0300

    Add files via upload
    A code for pwm signal reading by using arduino uno

---
+++ pwn signal reading.ino
@@ -0,0 +1,26 @@
+
+  int dir1pin = 2;
+  int dir2pin = 3;
+void setup() {
+  pinMode(6,OUTPUT);
+  pinMode(0, INPUT); 
+  Serial.begin(9600);
+  pinMode(dir1pin,OUTPUT);
+  pinMode(dir2pin,OUTPUT);
+}
+
+void loop() 
+{
+     
+     
+     double analogValue = analogRead(0);
+     Serial.println(analogValue*5/1023);
+     double motorValue = analogValue*255/1023;
+     delay(10);
+
+     digitalWrite(dir1pin, HIGH);
+     digitalWrite(dir2pin, LOW);
+     analogWrite(6, motorValue);
+  
+     
+}
